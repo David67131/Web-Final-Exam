a59a44f7d64f5ad6d67ac6efe6ca251ff2769648
David67131 <118649893+David67131@users.noreply.github.com>
2023-02-18 09:37:50 +0800

    2023218

---
+++ pages/api/stock/products/index.js
@@ -0,0 +1,32 @@
+import {connect, model, models, Schema} from "mongoose"
+const connectionString = 'mongodb+srv://user1:XwEofAuPYqSqIvZJ@cluster0.39ayzg6.mongodb.net/blogs'
+
+
+export default async function handler(req, res) {
+    await connect(connectionString);
+    console.log("req.method: ", req.method)
+
+    if (req.method === 'GET') {
+        const docs = await Article.find()
+        res.status(200).json(docs)
+    } else if (req.method === 'POST') {
+        console.log(req.body)
+        // res.status(200).json(req.body)
+        const doc = await Article.create(req.body)
+        res.status(201).json(doc)
+    } else {
+        res.setHeader('Allow', ['GET', 'POST'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+}
+
+
+
+const articleSchema = new Schema({
+    code: Number,
+    name: String,
+    price: Number,
+});
+
+console.log("Mongoose Models", models)
+const Article = models?.article || model('article', articleSchema);
